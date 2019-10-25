8ffe4a58a0c36fbc55ac61cd4ca1913a226e72cc
cyliu <chris.liu.hk@icloud.com>
2019-10-25 16:49:12 +0800

    init

--- src/main/java/csci4160/flinklab/TwitterExample.java
+++ src/main/java/csci4160/flinklab/TwitterExample.java
@@ -3,27 +3,14 @@ package csci4160.flinklab;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.configuration.Configuration;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
-import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.connectors.twitter.TwitterSource;
 import org.apache.flink.util.Collector;
-
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
-import org.java_websocket.server.WebSocketServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * <p>Usage: <code>Usage: TwitterExample [--output &lt;path&gt;]
@@ -54,9 +41,6 @@ public class TwitterExample {
         // emit result to the standard output
         SinkFunction<Tuple2<String, Integer>> printSink = new PrintSinkFunction<>();
         tweets.addSink(printSink).name("StdoutSink");
-        // emit result to the dashboard through websocket
-        SinkFunction<Tuple2<String, Integer>> webSocketSink = new WebsocketSinkFunction<>();
-        tweets.addSink(webSocketSink).name("WebSocketSink");
 
         // execute program
         env.execute("Twitter Streaming Example");
@@ -89,99 +73,6 @@ public class TwitterExample {
             }catch(Exception e) {
                 //System.out.println("No hashtag in tweets. Skip it ");
             }
-        }
-    }
-
-    public static class WebsocketSinkFunction<IN> extends RichSinkFunction<IN> {
-
-        private static final long serialVersionUID = 1L;
-
-        private transient static Logger log = LoggerFactory.getLogger(WebsocketSinkFunction.class);
-
-        private transient ObjectMapper objectMapper;
-
-        private transient MyWebSocketServer webSocketServer;
-
-        private int port = 18082;
-
-        @Override
-        public void open(Configuration parameters) throws Exception {
-            super.open(parameters);
-            this.log.info("Going to listern port:" + port);
-            webSocketServer = new MyWebSocketServer(new InetSocketAddress("0.0.0.0", port));
-            objectMapper = new ObjectMapper();
-            webSocketServer.start();
-        }
-
-        @Override
-        public void invoke(IN value, Context context) throws Exception {
-            webSocketServer.emit(objectMapper.writeValueAsString(value));
-        }
-
-        @Override
-        public String toString() {
-            return  WebsocketSinkFunction.class.toString();
-        }
-
-        @Override
-        public void close() throws Exception {
-            super.close();
-            this.webSocketServer.stop();
-        }
-
-        private static class MyWebSocketServer extends WebSocketServer {
-
-            private Set<WebSocket> websockets;
-
-            private Logger log = LoggerFactory.getLogger(WebSocketServer.class);
-
-            public MyWebSocketServer(InetSocketAddress address) {
-                super(address);
-                websockets = new HashSet<>();
-            }
-
-            @Override
-            public void onOpen(WebSocket conn, ClientHandshake handshake) {
-                websockets.add(conn);
-                this.log.info("new connection to " + conn.getRemoteSocketAddress());
-            }
-
-            @Override
-            public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-                websockets.remove(conn);
-                this.log.info("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
-            }
-
-            @Override
-            public void onMessage(WebSocket conn, String message) {
-                this.log.info("received message from "	+ conn.getRemoteSocketAddress() + ": " + message);
-            }
-
-            @Override
-            public void onMessage( WebSocket conn, ByteBuffer message ) {
-                this.log.info("received ByteBuffer from "	+ conn.getRemoteSocketAddress());
-            }
-
-            @Override
-            public void onError(WebSocket conn, Exception ex) {
-                System.err.println("an error occured on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
-            }
-
-            @Override
-            public void onStart() {
-                this.log.info("server started successfully");
-            }
-
-            public void emit(String string){
-                for(WebSocket websocket: this.websockets){
-                    try {
-                        websocket.send(string);
-                    }catch(Exception e){
-                        this.log.error("Fail to send data. Skip it",e);
-                    }
-                }
-            }
-
         }
     }
 }
